baa263536117e915890d7498071fbc7ceba31c54
San4ta <1152670339@qq.com>
2021-12-06 19:28:39 +0800

    添加项目文件。

---
+++ Board.h
@@ -0,0 +1,35 @@
+#ifndef _BOAED_H_
+#define _BOAED_H_
+
+#include <iostream>
+#include <graphics.h>
+#include <time.h>
+#include <string>
+//#include <conio.h>
+//手动调节
+constexpr auto WINDOS_X = 1600;										//窗口宽
+constexpr auto WINDOS_Y = 900;										//窗口高
+constexpr auto BOARD_SIZE = 800;									//棋盘大小
+constexpr auto LINE = 20;											//20x20格
+constexpr auto TIME = 30;											//30秒
+//AUTO
+constexpr auto BOARD_CORNER_X = (WINDOS_X - BOARD_SIZE) / 2;		//棋盘左上角坐标x
+constexpr auto BOARD_CORNER_Y = (WINDOS_Y - BOARD_SIZE) / 2;		//棋盘左上角坐标y
+constexpr auto GRID = BOARD_SIZE / LINE;							//格子边长
+
+class Board {
+public:
+	int record[LINE][LINE];			//棋盘上现有棋子 0无1黑2白
+	bool black;						//下棋顺序
+	Board();
+	void main_thread();             //主线程
+
+	void welcome();					//开始界面
+	void draw_board();				//绘制棋盘
+	void draw_time();				//30秒倒计时
+	void fight();					//下棋！
+	bool dice();					//骰子
+	bool judge(int x, int y);		//裁判
+};
+
+#endif // _BOAED_H_
---
+++ 五子棋.cpp
@@ -0,0 +1,53 @@
+﻿#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+	initgraph(640, 480);
+	BeginBatchDraw();
+
+	setlinecolor(WHITE);
+	setfillcolor(RED);
+
+	for (int i = 50; i < 600; i++)
+	{
+		circle(i, 100, 40);
+		floodfill(i, 100, WHITE);
+		FlushBatchDraw();
+		Sleep(1);
+		cleardevice();
+	}
+
+	EndBatchDraw();
+	closegraph();
+
+	//initgraph(1360, 900);
+	//initgraph(1600, 900);
+	//int i = 400, j = 50, k = 40;
+	//settextstyle(160, 80, _T("Arial"));
+	//BeginBatchDraw();
+	//while (1) {
+	//	int black, white = 0;
+
+	//	srand((unsigned)time(NULL));//随机数种
+
+	//	black = rand() % 6 + 1;	//骰子
+	//	i++;
+	//	char c = '0' + black;
+	//	outtextxy(1000, 40, c);
+	//	//Sleep(1);
+	//	FlushBatchDraw();
+	//	cleardevice();
+	//	white++;
+	//	if (white > 100)break;
+	//}
+	//EndBatchDraw();
+	//for (; i <= 400 + 800; i += k) {
+	//	line(i, j, i, j + 800);
+	//}
+	//i = 400;
+	//for (; j <= 50 + 800; j += k) {
+	//	line(i, j, i + 800, j);
+	//}
+	//closegraph();
+}
